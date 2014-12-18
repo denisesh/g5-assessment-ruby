84924f09a04a679b9a24a9ca7163a5b408f84660
denisesh <denisesh@gmail.com>
2014-12-18 12:00:46 -0800

    lots of errors

--- data/read_poem.rb
+++ data/read_poem.rb
@@ -1,4 +1,28 @@
-Dir.glob("**/*.txt") do |my_text_file|
-  p my_text_file
-  puts "working on: #{my_text_file}..."
+
+
+files = Dir.glob("**/*.txt")
+stats = {}
+
+lines = 0
+verses = 0
+
+files.each do |poem|
+  File.open(poem, "r") do |f|
+    f.readlines.each do |line|
+      if line != "\n"
+        lines += 1
+        if  line == "\n"
+          verses += 1
+        end
+      end
+    name_of_poem = File.open(f).readline
+    name_of_author = File.open(f).readline
+
+    end
+
+  end
+
+
+#  stats = {name_of_author =>{name_of_poem}=>{verses, lines}}}
+
 end
--- lib/file_spark.rb
+++ lib/file_spark.rb
@@ -1,8 +1,17 @@
-
-
-  Dir.glob("**/*.txt") do |my_text_file|
-    p my_text_file
-    puts "working on: #{my_text_file}..."
-  end
-
-  
+#
+#
+#   Dir.glob("**/*.txt") do |my_text_file|
+#     p my_text_file
+#     puts "working on: #{my_text_file}..."
+#   end
+#
+#
+#
+#
+#   First_text_file = File.open("poem_01.txt", "r")
+#
+#
+# need to count lines and break at /n
+# need to keep track of verses
+#
+# File IO vs File.open
